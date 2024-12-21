8f549739dccbb49299f0841aa27446aa69ed006a
Anas <107978854+dev-astech@users.noreply.github.com>
2024-12-21 14:59:59 +0530

    para screen

--- app/src/main/java/com/example/khulasaquran/screens/FirstScreen.kt
+++ app/src/main/java/com/example/khulasaquran/screens/FirstScreen.kt
@@ -1,71 +1,51 @@
-@file:Suppress("UNREACHABLE_CODE")
-
 package com.example.khulasaquran.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.khulasaquran.R
+
 @Composable
 fun FirstScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .background(color = Color(0xFF2F1E04))
+            .padding(2.dp)
     ) {
-        // Image Section
         Image(
-            painter = painterResource(id = R.drawable.home_image), // Replace with your drawable name
-            contentDescription = "Descriptive text for your image",
-            modifier = Modifier
-                .weight(1f)// Occupies the remaining space above the buttons
-                .fillMaxWidth() // Makes the image fill the width
+            painter = painterResource(R.drawable.home_image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth()
         )
-
-        // Buttons Section
-        Column(
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Button(
+            colors = ButtonColors(
+                containerColor = Color(0xD2B26536),
+                contentColor = Color.White,
+                disabledContentColor = Color.White,
+                disabledContainerColor = Color(0xD2B26536)
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {}
         ) {
-            Button(
-                onClick = { /* TODO: First Button Action */ },
-                modifier = Modifier.fillMaxWidth(),
-                colors = ButtonColors(
-                    contentColor = Color(0xD2B26536),
-                    containerColor = TODO(),
-                    disabledContainerColor = TODO(),
-                    disabledContentColor = TODO()
-                )
-            ) {
-                Text("Button 1")
-            }
-
-            Spacer(modifier = Modifier.height(8.dp)) // Space between buttons
-
-            Button(
-                onClick = { /* TODO: Second Button Action */ },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Button 2")
-            }
+            Text(
+                text = "Start",
+                color = Color.White
+            )
         }
     }
 }
--- app/src/main/java/com/example/khulasaquran/screens/ParaScreen.kt
+++ app/src/main/java/com/example/khulasaquran/screens/ParaScreen.kt
@@ -1,10 +1,41 @@
 package com.example.khulasaquran.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun ParaScreen(modifier: Modifier = Modifier) {
-    LazyColumn {  }
+fun ParaScreen() {
+    Column(
+        Modifier.background(color = Color(0xFF968B76)).fillMaxSize()
+    ) {
+        Text(
+            text = "Khulasa Quran",
+            color = Color.Black
+        )
+        LazyColumn(
+            Modifier.fillMaxWidth()
+        ) {
+            items(30) {
+                Text(
+                    text = "Para $it",
+                    color = Color.Black,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ParaScreenPreview(){
+    ParaScreen()
 }
