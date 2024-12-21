076740b996e3780056b54ce2867a5cf3d2f2f4b0
Anas <107978854+dev-astech@users.noreply.github.com>
2024-12-21 12:49:10 +0530

    welcome screen

---
+++ app/src/main/java/com/example/khulasaquran/screens/FirstScreen.kt
@@ -0,0 +1,13 @@
+package com.example.khulasaquran.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FirstScreen(modifier: Modifier = Modifier) {
+    Column {
+
+    }
+}
