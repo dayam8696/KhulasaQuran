96e5441a79c5530e403f73278fd447b98c06f8c4
Anas <107978854+dev-astech@users.noreply.github.com>
2024-12-21 12:57:00 +0530

    para screen

--- app/src/main/java/com/example/khulasaquran/screens/ParaScreen.kt
+++ app/src/main/java/com/example/khulasaquran/screens/ParaScreen.kt
@@ -1,2 +1,10 @@
 package com.example.khulasaquran.screens
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ParaScreen(modifier: Modifier = Modifier) {
+    LazyColumn {  }
+}
