5c46cdcd3f8802d4821b9e2ad074374b313eb11e
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-12-21 16:00:43 +0530

    error

--- app/src/main/java/com/example/khulasaquran/screens/MainActivity.kt
+++ app/src/main/java/com/example/khulasaquran/screens/MainActivity.kt
@@ -11,27 +11,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.khulasaquran.ui.theme.KhulasaQuranTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             KhulasaQuranTheme {
-                 KhulasaQuranApp()
-            }
-        }
-    }
 
-    @Composable
-    fun KhulasaQuranApp(modifier: Modifier = Modifier) {
-        val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = "first_screen") {
-            composable("first_screen") { FirstScreen(navController) }
-            composable("para_screen") { ParaScreen(navController) }
+            }
         }
     }
 }
