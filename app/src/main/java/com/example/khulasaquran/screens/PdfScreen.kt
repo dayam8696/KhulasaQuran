d4e6016f4d01fcf1d917f3a10e6c3576b3e026c0
Anas <107978854+dev-astech@users.noreply.github.com>
2024-12-22 12:46:57 +0530

    bug

--- app/src/main/java/com/example/khulasaquran/screens/PdfScreen.kt
+++ app/src/main/java/com/example/khulasaquran/screens/PdfScreen.kt
@@ -1,7 +1,13 @@
 package com.example.khulasaquran.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import java.lang.reflect.Modifier
 
+
+@Composable
+fun PdfViewer() {
+
+}
