653cd546812acc0df82bdfd07e96156109e8d41e
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-12-22 12:21:25 +0530

    added pdfview

--- app/src/main/java/com/example/khulasaquran/screens/FirstScreen.kt
+++ app/src/main/java/com/example/khulasaquran/screens/FirstScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.khulasaquran.R
 
 @Composable
@@ -82,6 +83,10 @@ fun FirstScreen(navController: NavController) {
 }
 
 
-
+@Preview(showBackground = true)
+@Composable
+fun firstscreenPreview() {
+    FirstScreen(navController = rememberNavController())
+}
 
 
---
+++ app/src/main/java/com/example/khulasaquran/screens/PdfScreen.kt
@@ -0,0 +1,7 @@
+package com.example.khulasaquran.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import java.lang.reflect.Modifier
+
